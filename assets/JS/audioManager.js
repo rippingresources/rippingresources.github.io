707fb0ad2c6cc12d79a991dfcb867c5894dc38dc
KLanausse <lanausse@lanausse.com>
2025-09-02 02:27:32 -0400

    Mute Button

--- assets/JS/audioManager.js
+++ assets/JS/audioManager.js
@@ -1,10 +1,28 @@
+//TODO: Grab music from spreadsheet
+//TODO: List music from previous days
+//TODO: Show song metadata
+//TODO: Check cookies to see if the bgm should say muted on first click.
+//
+
+let hasClickedEver = false
 let playing = false
+
 
 $(document).ready(function(){
     $("html").click(function(){
-        if (!playing) {
+        if (!hasClickedEver) {
+            hasClickedEver = true
             playing = !playing
             $("#bgm")[0].play();
         }
+    });
+
+    $('#mute').click(function() {
+        if (playing) {
+            $("#bgm")[0].pause();
+        } else {
+            $("#bgm")[0].play();
+        }
+        playing = !playing
     });
 });
