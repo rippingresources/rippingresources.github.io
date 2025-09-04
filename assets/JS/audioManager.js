d72f5f336e487a95bbb1a89c2f73f31ef6819473
WindowsOnAMac <lanausse@lanausse.com>
2025-09-04 18:48:31 -0400

    Pull BGM from Submissions

--- assets/JS/audioManager.js
+++ assets/JS/audioManager.js
@@ -6,13 +6,26 @@
 
 let hasClickedEver = false
 let playing = false
+const sheet = "https://docs.google.com/spreadsheets/d/e/2PACX-1vT9AQJRlqMIk6MdjmW_-4pMoWrRZqY1tUJou5qbi5pbJTPDXa8yq-SPvC_90BbPUJChYfVhMMc1oHFt/pub?gid=1266918037&single=true&output=csv";
 
+async function getData() {
+    let req = await fetch(sheet);
+    let csv = await req.text();
+    return Papa.parse(csv, { header: true }).data;
+}
 
-$(document).ready(function(){
+$(document).ready(async function(){
+
+    let bgmData = await getData();
+    console.log(bgmData, bgmData.length);
+    let randomBgm = bgmData[Math.floor(Math.random()*bgmData.length)];
+    $("#bgm")[0].src = randomBgm.link;
+
     $("html").click(function(){
         if (!hasClickedEver) {
             hasClickedEver = true
             playing = !playing
+            $("#song-title").text(`${randomBgm.title} - ${randomBgm.author}`);
             $("#bgm")[0].play();
         }
     });
@@ -26,3 +39,4 @@ $(document).ready(function(){
         playing = !playing
     });
 });
+
